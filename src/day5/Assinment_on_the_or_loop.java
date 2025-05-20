7348973dbdfacbdb4f82fa66985c1d3c4e251956
chiddarwargauri <chiddarwargauri@gmail.com>
2025-05-20 23:13:57 +0530

    practicing for loop

---
+++ src/day5/Assinment_on_the_or_loop.java
@@ -0,0 +1,58 @@
+package day5;
+
+public class Assinment_on_the_or_loop {
+	
+	
+	
+	public static void main(String[] args) {
+		
+
+	  
+		
+		
+		
+	}
+	
+	
+
+	public class Number{
+		
+	public static void main(String[] args)
+	{
+		
+		
+	 for (int i=1; i<=10;i++) {
+		 
+		 System.out.println(i);
+	 }
+		
+	}
+	
+	
+	
+	
+	
+	//-------------------------------------------------------
+	
+	
+	
+	
+	
+	
+	public class FiveMultiple{
+		
+		public static void main(String[] args) {
+			for(int i = 1 ; i<10;i++) {
+				
+				
+				int five=i*5;
+				
+				
+				System.out.println(five);
+			}
+		}
+	}
+	}
+
+
+}
