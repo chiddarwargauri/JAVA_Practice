c361c747ef2c2755889115207be1d69d099f03f4
chiddarwargauri <chiddarwargauri@gmail.com>
2025-06-11 22:00:19 +0530

    Day 10 --> Wrapper class  uses

--- src/day10/WrapperClass.java
+++ src/day10/WrapperClass.java
@@ -1,60 +1,47 @@
 package day10;
 
+import java.util.ArrayList; // ✅ Add this import
+
 public class WrapperClass {
 
-	
-	public static void main(String[] args) {
-		
-		// 1. Converting primitive  data to object type (Auto Boxing)
-		
-		int i = 10;
-		double d= 10.5;
-		char c= 'A';
-		boolean b= true;
-		
-		//Integer iobj= new Integer(i);---- deprecate 
-		Integer iobj =  i;
-		
-		System.out.println(iobj);
-		
-		
-		double dobj=d;
-		
-		System.out.println(dobj);
-		
-		Character cobj = c;
-		
-	         System.out.println(cobj);
-	         
-	         Boolean bobj = b;
-	         
-	         System.out.println(bobj);
-		
-	         
-	         // 2. Converting the object type of data into primitive type ---- up boxing 
-	         
-	         
-	         int intvalue=iobj ;
-	         
-	         double doublevalue = dobj;
-	         
-	         char charvalue= cobj;
-	         
-	         boolean booleanvalue = bobj ;
-	         
-	         
-	         System.out.println(intvalue);
-	         
-	         System.out.println(doublevalue);
+    public static void main(String[] args) {
 
-	         
-	         System.out.println(charvalue);
+        // 1. Converting primitive data to object type (Auto Boxing)
+        int i = 10;
+        double d = 10.5;
+        char c = 'A';
+        boolean b = true;
 
-	         
-	         System.out.println(booleanvalue);
+        Integer iobj = i; // Auto-boxing
+        System.out.println(iobj);
 
-	                
-		
-		
-	}
+        Double dobj = d; // Auto-boxing (use Double instead of just double)
+        System.out.println(dobj);
+
+        Character cobj = c;
+        System.out.println(cobj);
+
+        Boolean bobj = b;
+        System.out.println(bobj);
+
+        // 2. Converting object to primitive (Unboxing)
+        int intvalue = iobj;
+        double doublevalue = dobj;
+        char charvalue = cobj;
+        boolean booleanvalue = bobj;
+
+        System.out.println(intvalue);
+        System.out.println(doublevalue);
+        System.out.println(charvalue);
+        System.out.println(booleanvalue);
+
+        // 3. Storing null
+        // int x = null; // ❌ Not allowed
+        Integer x = null; // ✅ Allowed
+
+        // 4. Collections allow only object (not primitives)
+        // Arrayist ar = new ArrayList(); // ❌ Typo: "Arrayist"
+
+        ArrayList<Integer> ar = new ArrayList<>(); // ✅ Correct
+    }
 }
