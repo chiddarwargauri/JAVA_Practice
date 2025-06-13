75ec7c38ad3a9db55d0240377fa083dafb7db963
chiddarwargauri <chiddarwargauri@gmail.com>
2025-06-13 22:01:17 +0530

    Day 11- class and object -----Assignments Question solution

--- src/day11/Assignment1.java
+++ src/day11/Assignment1.java
@@ -1,5 +1,33 @@
 package day11;
 
-public class Assignment1 {
+public class Assignment1{
 
+	
+	/*Assignment 1: Basic Class and Object Creation
+	• Objective: Create a simple class with attributes and methods.
+	• Task:
+	o Define a class named Car with attributes: make, model, and year.
+	o Create a method displayInfo() that prints the details of the car.
+	o In the main method, create an object of the Car class, set values for the attributes,
+	and call the displayInfo() method to display the car’s details.
+*/
+	
+	
+	public static void main(String[] args) {
+		//---------------------Question No:- 1--------------------
+		/*Car c = new Car();
+		c.make="A.S>Manakar";
+		c.model="Defenser";
+		c.year=2020;
+		c.displayInfo();*/
+		
+		
+		
+		//-------------------------Question No:- 2----------------------
+         Car c2=new Car("GAuri" ,"Defeser" ,2020);
+		
+		 c2.displayInfo();
+		
+
+	}
 }
---
+++ src/day11/Car.java
@@ -0,0 +1,40 @@
+package day11;
+
+public class Car {
+
+	
+
+		
+
+		//Attribute
+		
+		String make;
+		String model;
+		int year;
+		
+		//Method
+		
+		
+		
+		///1...........
+		
+		
+		void displayInfo() {
+			
+			System.out.println(" make:-"+make+" "+" model:- " +model+ " "+"Year:- "+year);
+		}
+		
+		//2...........
+		
+		Car(String M1,String m2,int y){
+			
+			make=M1;
+			model=m2;
+			year=y;
+		}
+
+		
+		
+		
+	
+}
