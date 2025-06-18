4a39ef5b6a6d47c4598eab9bcb4072e96369383d
chiddarwargauri <chiddarwargauri@gmail.com>
2025-06-18 22:29:58 +0530

    different types of method day 12

--- src/day12/BankAccount.java
+++ src/day12/BankAccount.java
@@ -7,28 +7,100 @@ public class BankAccount {
 			//2. No parameter and return value
 			//3. takes parameter and no return values 
 			//4. Takes parameters and Returns a values
-			
-			
+		
+	//-------------------------------------------
+	// variables
+	
 	
 	double balance;
 	
 	double interestRate;
+	
+	//--------------------------------------------
+	
 	//constructor
 	
-	 BankAccount(double bal, double interest){
+	BankAccount(double bal, double interest){
 		 balance=bal;
 		 interestRate=interest;
 		 
 		 
 	 }
-	
+	 
+	//--------------------------------------
 	// Methods
-	public static void main (String args) 
-	{
-		
-		
-		
+	       //------------------
 	
-		
+	          //1. No parameter and no return value
+
+	 
+	 void checkBalance() {
+		 System.out.println("Balance:= "+balance);
+	 }
+	 
+	 
+	          //-------------------------------------
+	 
+	         		//2. No parameter and return value
+	 
+	 double getInterestRate() {
+		 
+		 return interestRate;
+				 
+	 }
+	 
+	       //==========================================
+		      //3. takes parameter and no return values 
+    
+        void deposit(double amount) {
+        	
+        	if(amount >0) {
+        		balance+=amount;
+        		System.out.println("Diposit amount:-"+amount);
+        	}
+        	else {
+        		System.out.println("amount must be greater than zero / amount must be positive");
+        	}
+        }
+        
+            //==========================================
+        
+                // 			//4. Takes parameters and Returns a values
+
+        boolean withdraw(double amount) {
+        	if(amount>0 && amount<=balance) {
+        		
+        		balance-=amount;
+        		System.out.println("withdraw amountL-" +amount);
+        		
+        		return true;
+        	}
+        	else {
+        		
+        		System.out.println("Insufficent ammount");
+        		return false;
+        	}
+        }
+	 // main method
+	 
+	public static void main (String args[]) 
+	{
+		BankAccount obj= new BankAccount(120.23,56.26);
+              obj.checkBalance();
+		//====================================
+     	double store_interest_rate	=obj.getInterestRate();// if  any method return somethind must be stored it into variable
+	    System.out.println("Interest rate is "+store_interest_rate);
+		//==========================================
+	    
+	    obj.deposit(235.98);
+	    obj.checkBalance();
+	     //==========================================
+	    
+	    boolean withrawresult=obj.withdraw(56.0);
+	    
+	   if (withrawresult==true) {
+		   obj.checkBalance();
+	   }
+	 
 	}
 }
